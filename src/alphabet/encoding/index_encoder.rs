e9d7d5bdd8c5d5a924bb1766442807fc371f256c
Prometheus <cryan118@gmail.com>
2019-03-08 05:43:07 +1000

    Made IndexEncoder into AsciiIndexEncoder and changed AlphabetEncoder to return Result

--- src/alphabet/encoding/index_encoder.rs
+++ src/alphabet/encoding/index_encoder.rs
@@ -4,51 +4,57 @@
 pub use super::AlphabetEncoder;
 
 use crate::alphabet::Alphabet;
+use crate::alphabet::encoding::{EncodingError, ErrorKind};
+use super::EncodingResult;
 use bimap::{BiHashMap, Overwritten};
 
 /// An index encoder takes each symbol of an alphabet and encodes it based on its index in the slice
-/// returned from the [Alphabet::symbols()](super::Alphabet::symbols) method.
+/// returned from the [Alphabet::symbols()](super::Alphabet::symbols) method. The Ascii part of the
+/// name derives from the fact that it always uses one byte and can thus only map 256 different symbols.
 ///
 /// # Notes
 /// This is the default encoder and should work fine in most cases.
 ///
 /// It makes use of the [Alphabet::max_alphabet_size()](super::Alphabet::max_alphabet_size) method
-/// to determine the number of bytes that will be needed to encode all symbols.
+/// to determine if it can be safely used.
 ///
 /// If you expect the number of symbols in your [Alphabet](super::Alphabet) will not be static
-/// you may have to create your own encoder.
-pub struct IndexEncoder<'a, A: Alphabet> {
+/// you may have to create your own encoder or you can try using the
+/// [recalculate_mapping()](AsciiIndexEncoder::recalculate_mapping) method.
+pub struct AsciiIndexEncoder<'a, A: Alphabet> {
     alphabet: &'a A,
-    mapping: BiHashMap<&'a str, Vec<u8>>,
+    mapping: BiHashMap<&'a str, u8>,
 }
 
-impl<'a, A: Alphabet> IndexEncoder<'a, A> {
+impl<'a, A: Alphabet> AsciiIndexEncoder<'a, A> {
     /// Construct a new [IndexEncoder] from a given alphabet.
-    pub fn new(alphabet: &'a A) -> IndexEncoder<'a, A> {
-        let mapping = IndexEncoder::construct_mapping(alphabet);
+    pub fn new(alphabet: &'a A) -> AsciiIndexEncoder<'a, A> {
+        let mapping = AsciiIndexEncoder::construct_mapping(alphabet);
 
-        IndexEncoder { alphabet, mapping }
+        AsciiIndexEncoder { alphabet, mapping }
     }
 
     /// Recalculates the mapping. Must be called if the Alphabet is ever altered such that a symbol
     /// changes or the number of symbols changes.
     pub fn recalculate_mapping(&mut self) {
-        self.mapping = IndexEncoder::construct_mapping(self.alphabet);
+        self.mapping = AsciiIndexEncoder::construct_mapping(self.alphabet);
     }
 
     /// Constructs the mapping from symbols in the alphabet to bytes
-    fn construct_mapping(alphabet: &A) -> BiHashMap<&str, Vec<u8>> {
+    fn construct_mapping(alphabet: &A) -> BiHashMap<&str, u8> {
         let max_size = alphabet.max_alphabet_size() as u64;
-        let generator = ByteIdGenerator::from_max(max_size);
+        let num_symbols = alphabet.symbols().len();
+        if max_size > 256 || alphabet.symbols().len() > 256 {
+            panic!(
+                "This alphabet expects to/has more symbols than can the AsciiIndexEncoder is \
+                capable of mapping. Try using UnicodeIndexEncoder instead."
+            )
+        }
 
-        let mut mapping = BiHashMap::with_capacity(alphabet.symbols().len());
+        let mut mapping = BiHashMap::with_capacity(num_symbols);
 
         for (index, symbol) in alphabet.symbols().iter().enumerate() {
-            let id = generator
-                .get_id(index as u64)
-                .expect("Alphabet size inconsistency");
-
-            if mapping.insert(*symbol, id) != Overwritten::Neither {
+            if mapping.insert(*symbol, index as u8) != Overwritten::Neither {
                 panic!("Alphabet with symbols {:?} contains duplicate symbol.", alphabet.symbols());
             }
         }
@@ -57,39 +63,56 @@ impl<'a, A: Alphabet> IndexEncoder<'a, A> {
     }
 }
 
-impl<'a, A: Alphabet + 'a> AlphabetEncoder<A> for IndexEncoder<'a, A> {
-    fn encode(&self, symbol: &str) -> Vec<u8> {
+impl<'a, A: Alphabet> AlphabetEncoder<A> for AsciiIndexEncoder<'a, A> {
+    fn encode(&self, symbol: &str) -> EncodingResult<Vec<u8>> {
         let res = self.mapping.get_by_left(&symbol);
 
         // Check if a mapping was found if not determine the error and panic! with useful message
         if let Some(encoded) = res {
-            return encoded.to_owned();
+            Ok(vec![*encoded])
         } else {
-            let mut error_message = String::from("IndexEncoder failed to encode symbol.");
+            let mut error_message
+                = String::from("AsciiIndexEncoder failed to encode symbol. ");
 
             // If the symbol is not in the alphabet:
-            if !self.alphabet.contains(symbol) {
+            let kind = if !self.alphabet.contains(symbol) {
                 let extra = format!(
-                    " The input to encode() was a symbol which does not exist in the alphabet: {}",
+                    "The input to encode() was a symbol which does not exist in the alphabet: {}",
                     symbol
                 );
 
                 error_message.push_str(&extra);
-            } else {
-                error_message.push_str(
-                    " Did you alter the alphabet and forget to call recalculate_mapping()?"
-                )
-            }
 
-            panic!(error_message);
-        };
+                ErrorKind::InvalidSymbol(symbol.to_owned())
+            } else { // Symbol is in the Alphabet but wasn't when the mapping was constructed
+                error_message.push_str(
+                    "Did you alter the alphabet and forget to call recalculate_mapping()?"
+                );
+
+                ErrorKind::NoMapping
+            };
+
+            Err(EncodingError::new(kind, error_message))
+        }
     }
 
-    fn decode(&self, symbol: &[u8]) -> &str {
-        self.mapping
-            .get_by_right(&symbol.to_vec())
-            .expect("IndexEncoder failed to decode symbol. \
-                     Did you alter the alphabet and forget to call recalculate_mapping()?")
+    fn decode(&self, symbol: &[u8]) -> EncodingResult<&str> {
+        if symbol.len() != 1 {
+            let kind = ErrorKind::InvalidBytes(symbol.to_vec());
+            let desc = "AsciiIndexEncoder received more than one byte to decode.";
+            return Err(EncodingError::new(kind, desc.to_owned()));
+        };
+
+        let res = self.mapping.get_by_right(&symbol[0]);
+
+        if let Some(decoded) = res {
+            Ok(decoded)
+        } else {
+            let kind = ErrorKind::NoMapping;
+            let desc = "AsciiIndexEncoder failed to decode symbol. \
+            Did you alter the alphabet and forget to call recalculate_mapping()?";
+            Err(EncodingError::new(kind, desc.to_owned()))
+        }
     }
 }
 
@@ -121,22 +144,26 @@ mod tests {
     #[test]
     fn correct_encode() {
         let a = TestAlphabet;
-        let encoder = IndexEncoder::new(&a);
+        let encoder = AsciiIndexEncoder::new(&a);
 
         let seq   = vec!["AA", "BB", "CC", "AA", "BB"];
         let encoded= vec![0, 1, 2, 0, 1];
 
-        assert_eq!(encoder.encode_all(&seq), encoded);
+        assert_eq!(encoder.encode_all(&seq).unwrap(), encoded);
     }
 
     #[test]
-    #[should_panic(expected="IndexEncoder failed to encode symbol. The input to encode() was a symbol which does not exist in the alphabet: A")]
     fn unexpected_symbol() {
         let a = TestAlphabet;
-        let encoder = IndexEncoder::new(&a);
+        let encoder = AsciiIndexEncoder::new(&a);
 
         let seq = vec!["AA", "BB", "A"];
-        encoder.encode_all(&seq);
+        let res = encoder.encode_all(&seq);
+
+        match res {
+            Ok(_) => panic!("Encoding worked when there was an invalid symbol"),
+            Err(err) => assert_eq!(*err.kind(), ErrorKind::InvalidSymbol("A".to_owned())),
+        }
     }
 
     #[test]
--- src/alphabet/encoding/mod.rs
+++ src/alphabet/encoding/mod.rs
@@ -4,24 +4,89 @@
 pub mod index_encoder;
 
 pub use super::Alphabet;
+use std::error::Error;
+use std::fmt;
+
+pub type EncodingResult<T> = Result<T, EncodingError>;
 
 pub trait AlphabetEncoder<A: Alphabet> {
     /// Takes in a symbol from the [Alphabet](super::Alphabet) A and turns it into a vector of bytes
-    fn encode(&self, symbol: &str) -> Vec<u8>;
+    fn encode(&self, symbol: &str) -> EncodingResult<Vec<u8>>;
 
     /// Reverses [encode()](AlphabetEncoder::encode())
-    fn decode(&self, symbol: &[u8]) -> &str;
+    fn decode(&self, symbol: &[u8]) -> EncodingResult<&str>;
+
+    /// How many bytes you expect an encoded symbol to take on average.
+    /// Does not have to be exact and is purely for extra efficiency in memory allocation.
+    ///
+    /// # Default
+    /// Defaults to 1 byte per encoded symbol.
+    #[inline]
+    fn size_hint(&self) -> usize {
+        1
+    }
 
     /// Takes a slice of strings and encodes them all using [encode()](AlphabetEncoder::encode()).
-    /// Returns a flattened vec of the encoded strings.
-    fn encode_all(&self, symbols: &[&str]) -> Vec<u8> {
-        let mut encoded = Vec::with_capacity(symbols.len());
+    /// Returns a flattened vec of the encoded strings on success.
+    fn encode_all(&self, symbols: &[&str]) -> EncodingResult<Vec<u8>> {
+        // We make the guess that each symbol will take one byte when encoded
+        let mut encoded = Vec::with_capacity(symbols.len() * self.size_hint());
 
         for symbol in symbols {
-            let encode = self.encode(*symbol);
+            let encode = self.encode(*symbol)?;
             encoded.extend_from_slice(encode.as_slice());
         }
 
-        encoded
+        Ok(encoded)
+    }
+}
+
+/// Represents the kind of error that occurred while encoding or decoding an alphabet symbol.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ErrorKind {
+    /// The symbol passed to [encode()](AlphabetEncoder::encode) was not in the Alphabets dictionary
+    /// or the symbol does not have a mapping in that encoder for some reason.
+    InvalidSymbol(String),
+
+    /// The bytes passed to [decode()](AlphabetEncoder::decode) were invalid.
+    InvalidBytes(Vec<u8>),
+
+    /// The encoder has no way to map your symbol/bytes. Potentially because the alphabet has
+    /// changed. Each encoder will have different ways of handling this and different reasons as to
+    /// why it might occur.
+    NoMapping,
+
+    /// Some other error occurred (check description/display)
+    Other
+}
+
+#[derive(Debug, Clone)]
+pub struct EncodingError {
+    kind: ErrorKind,
+    description: String,
+}
+
+impl EncodingError {
+    /// Construct a new EncodingError from the given ErrorKind and description
+    pub fn new(kind: ErrorKind, description: String) -> EncodingError {
+        EncodingError { kind, description }
+    }
+
+    /// Get the associated ErrorKind for this error
+    pub fn kind(&self) -> &ErrorKind {
+        &self.kind
+    }
+
+    /// Get the associated description for this error
+    pub fn description(&self) -> &str {
+        &self.description
+    }
+}
+
+impl Error for EncodingError {}
+
+impl fmt::Display for EncodingError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Encoding error occurred with description:\n\t{}", self.description)
     }
 }
